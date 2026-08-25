998a92cc373a56357cf4dcd82066d706891547d4
cmd-hmn <hafizmuhammadnoor01@gmail.com>
2026-08-25 11:41:58 +0500

    feat(quantization): implement QData struct with Default trait for QTYPE

--- rust-core/quantization/quantize.rs
+++ rust-core/quantization/quantize.rs
@@ -1,6 +1,25 @@
+// Default will be Float32
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum QTYPE {
     Binary,
     Int8,
     Float32
+}
+
+// default is f32 
+impl Default for QTYPE {
+    fn default() -> Self {
+        QTYPE::Float32
+    }
+}
+
+#[derive(Default, Debug)]                                                                                                                       
+pub struct QData {
+    qtype: QTYPE,
+    data: Vec<QTYPE>,
+    scale: Vec<QTYPE>
+}
+
+fn qf32_to_qi8(value: f32) -> QData {
+     QData::default()
 }
